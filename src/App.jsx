c2407c2d01bab7d92af1ac72c2d13b8c1fe2a473
KraYFS <grigorievdaniil13@gmail.com>
2024-11-04 13:44:10 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MainPage from './pages/mainPage/mainPage'
+import './shared/styles/index.css'
+
+function App() {
+  return (
+    <div>
+      <MainPage />
+    </div>
+  )
+}
+
+export default App
