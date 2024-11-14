80d1ec714ae675aa1f17fc64675bed13df6e58dd
KraYFS <grigorievdaniil13@gmail.com>
2024-11-14 02:28:09 +0200

    change url

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const baseURL =  "https://csworldfreelance.netlify.app";
+  const baseURL =
+    mode === "local-dev" ? "http://localhost:3000" : "https://csworldfreelance.netlify.app";
 
   return {
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: "http://127.0.0.1:8000",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, "/api"),
+        },
+      },
+    },
     plugins: [react()],
     define: {
       BaseUrl: JSON.stringify(baseURL),
