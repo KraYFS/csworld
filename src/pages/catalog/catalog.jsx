1eb5566632581ffb6b6254e26f524a3a4dc9f54c
KraYFS <grigorievdaniil13@gmail.com>
2024-11-14 04:26:37 +0200

    test

--- src/pages/catalog/catalog.jsx
+++ src/pages/catalog/catalog.jsx
@@ -13,16 +13,16 @@ import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 const Catalog = () => {
-    // const [data, setData] = useState(null)
-    // const { name } = useParams()
+    const [data, setData] = useState(null)
+    const { name } = useParams()
 
-    // useEffect(() => {
-    //     fetch(`${__BASE_URL__}/api/${name}`)
-    //         .then((res) => res.json())
-    //         .then((data) => {
-    //             setData(data);
-    //         });
-    // }, [name])
+    useEffect(() => {
+        fetch(`${__BASE_URL__}/api/${name}`)
+            .then((res) => res.json())
+            .then((data) => {
+                setData(data);
+            });
+    }, [name])
 
     const { t, i18n } = useTranslation();
 
@@ -60,9 +60,9 @@ const Catalog = () => {
                             <WeaponCategoryElem fontSize='36' textTransform='uppercase' fontWeight='500' name='awp' img={img} />
                         </div>
                         <div className={styles.WeaponCategoryCards}>
-                            {/* {data.map(item => {
+                            {data.map(item => {
                                 return <WeaponCategoryCard link={`catalog card/weapon models/${item._id}`} key={item._id} content={item.content} title={item.title} img={item.pictures[0]} />
-                            })} */}
+                            })}
                         </div>
                     </section>
                 </div>
