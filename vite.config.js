884a88b816bbbc92a1b07b6c05c2717db13fa55d
KraYFS <grigorievdaniil13@gmail.com>
2024-11-14 02:21:30 +0200

    change url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  baseURL = '/'
+  const baseURL =  "https://csworldfreelance.netlify.app";
 
   return {
     plugins: [react()],
