5f13a2c0bbe991839ba86244025c3b449f7f59d0
KraYFS <grigorievndaniil13@gmail.com>
2024-12-16 17:19:47 +0200

    added paragraph in info page

--- src/pages/catalogCard/catalogCard.jsx
+++ src/pages/catalogCard/catalogCard.jsx
@@ -52,97 +52,97 @@ const CatalogCard = () => {
 
     const checkParagraph = (text) => {
         return text.split('%$20').map((paragraph, index) => {
-        return <React.Fragment key={index}>
-            {paragraph}
-            <br />
-        </React.Fragment>
-    })
+            return <React.Fragment key={index}>
+                {paragraph}
+                <br />
+            </React.Fragment>
+        })
     }
 
-if (!data) {
-    return (
-        <Loader />
-    )
-} else {
-    return (
-        <div>
-            <Header />
-            <div className="container">
-                <section className="content">
-                    <div className={styles.mainPage_inner}>
-                        <div className={styles.swiper_gallery}>
-                            <Swiper
-                                modules={[Thumbs, Navigation]}
-                                thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
-                                navigation
-                                watchSlidesProgress
-                                pagination={{ clickable: true }}
-                                slidesPerView={1}
-                                className={styles.swiper}
-                            >
-                                {data.pictures.map(img => {
-                                    return (<SwiperSlide><img style={{ width: '500px' }} src={img} alt="" /></SwiperSlide>)
-                                })}
-                            </Swiper>
-                            <Swiper
-                                modules={[Thumbs, Pagination]}
-                                pagination={{ clickable: true }}
-                                onSwiper={setThumbsSwiper}
-                                slidesPerView={3}
-                                style={{ width: '100%', height: "96px", marginTop: "18px" }}
-                            >
-                                {data.pictures.map(img => {
-                                    return (<SwiperSlide><img style={{ width: '162px', height: '95px', borderRadius: '5px' }} src={img} alt="" /></SwiperSlide>)
-                                })}
-                            </Swiper>
-                        </div>
-                        <div>
-                            <SectionTitle title={i18n.language === 'ua' ? data.titleSecondLang : data.title} />
-                            <SeoText maxWidth='1000px' backColor={false} text={i18n.language === 'ua' ? data.descriptionSecondLang : data.description} />
-                            <div className={styles.peculiarities}>
-                                {i18n.language === 'ua' ? data.tagsSecondLang.map((text) => {
-                                    return (<Peculiarities text={text} />)
-                                }) : data.content.map((text) => {
-                                    return (<Peculiarities text={text} />)
-                                })}
+    if (!data) {
+        return (
+            <Loader />
+        )
+    } else {
+        return (
+            <div>
+                <Header />
+                <div className="container">
+                    <section className="content">
+                        <div className={styles.mainPage_inner}>
+                            <div className={styles.swiper_gallery}>
+                                <Swiper
+                                    modules={[Thumbs, Navigation]}
+                                    thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
+                                    navigation
+                                    watchSlidesProgress
+                                    pagination={{ clickable: true }}
+                                    slidesPerView={1}
+                                    className={styles.swiper}
+                                >
+                                    {data.pictures.map(img => {
+                                        return (<SwiperSlide><img style={{ width: '500px' }} src={img} alt="" /></SwiperSlide>)
+                                    })}
+                                </Swiper>
+                                <Swiper
+                                    modules={[Thumbs, Pagination]}
+                                    pagination={{ clickable: true }}
+                                    onSwiper={setThumbsSwiper}
+                                    slidesPerView={3}
+                                    style={{ width: '100%', height: "96px", marginTop: "18px" }}
+                                >
+                                    {data.pictures.map(img => {
+                                        return (<SwiperSlide><img style={{ width: '162px', height: '95px', borderRadius: '5px' }} src={img} alt="" /></SwiperSlide>)
+                                    })}
+                                </Swiper>
                             </div>
-                            <div className={styles.btns}>
-                                {name !== 'posts'
-                                    ?
-                                    <>
-                                        <DownloadBtn click={downloadFile} backColor='#6D86FF' text={t('downloadBtn')} />
-                                    </>
-                                    : null}
-                                {name === 'assemblies' && (
-                                    <>
-                                        <DownloadBtn click={downloadTorrentFile} backColor='#54AB64' text={t('downloadBtn')} />
-                                    </>
-                                )}
+                            <div>
+                                <SectionTitle title={i18n.language === 'ua' ? data.titleSecondLang : data.title} />
+                                <SeoText maxWidth='1000px' backColor={false} text={i18n.language === 'ua' ? data.descriptionSecondLang : data.description} />
+                                <div className={styles.peculiarities}>
+                                    {i18n.language === 'ua' ? data.tagsSecondLang.map((text) => {
+                                        return (<Peculiarities text={text} />)
+                                    }) : data.content.map((text) => {
+                                        return (<Peculiarities text={text} />)
+                                    })}
+                                </div>
+                                <div className={styles.btns}>
+                                    {name !== 'posts'
+                                        ?
+                                        <>
+                                            <DownloadBtn click={downloadFile} backColor='#6D86FF' text={t('downloadBtn')} />
+                                        </>
+                                        : null}
+                                    {name === 'assemblies' && (
+                                        <>
+                                            <DownloadBtn click={downloadTorrentFile} backColor='#54AB64' text={t('downloadBtn')} />
+                                        </>
+                                    )}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    {
-                        name === 'assemblies'
-                            ? <Spoiler title={t('systemRequirements')} text={checkParagraph(data.systemRequirements)} />
-                            : name === 'posts'
-                                ? <Spoiler title="Автор" text={data.author} />
-                                : <Spoiler title={t('howDownload')} text={i18n.language === 'ua' ? checkParagraph(data.systemRequirementsSecondLang) : checkParagraph(data.systemRequirements)} />
-                    }
-                    {
-                        name === 'assemblies'
-                            ? null
-                            : name === 'posts'
-                                ? <Spoiler post="true" image text={checkParagraph(data.postText)} />
-                                : name === 'maps'
-                                    ? <Spoiler title={t('systemRequirements')} text={checkParagraph(data.systemRequirements)} />
-                                    : null
-                    }
-                </section>
+                        {
+                            name === 'assemblies'
+                                ? <Spoiler title={t('systemRequirements')} text={checkParagraph(data.systemRequirements)} />
+                                : name === 'posts'
+                                    ? <Spoiler title="Автор" text={data.author} />
+                                    : <Spoiler title={t('howDownload')} text={i18n.language === 'ua' ? checkParagraph(data.systemRequirementsSecondLang) : checkParagraph(data.systemRequirements)} />
+                        }
+                        {
+                            name === 'assemblies'
+                                ? null
+                                : name === 'posts'
+                                    ? <Spoiler post="true" image text={checkParagraph(data.postText)} />
+                                    : name === 'maps'
+                                        ? <Spoiler title={t('systemRequirements')} text={checkParagraph(data.systemRequirements)} />
+                                        : null
+                        }
+                    </section>
+                </div>
+                <Footer />
             </div>
-            <Footer />
-        </div>
-    );
-}
+        );
+    }
 }
 
 export default CatalogCard;
--- src/pages/informationPage/informationPage.jsx
+++ src/pages/informationPage/informationPage.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Footer from "../../components/footer/footer";
 import Header from "../../components/header/header";
@@ -9,6 +10,15 @@ const InformationPage = () => {
     const { name } = useParams()
     const { t, i18n } = useTranslation();
 
+    const checkParagraph = (text) => {
+        return text.split('%$20').map((paragraph, index) => {
+            return <React.Fragment key={index}>
+                {paragraph}
+                <br />
+            </React.Fragment>
+        })
+    }
+
     if (name === 'User agreement') {
         return (
             <div>
@@ -16,7 +26,7 @@ const InformationPage = () => {
                 <div className="container">
                     <section className="content">
                         <SectionTitle title={t('userAgreementTitle')} />
-                        <Spoiler post text={t('userAgreementText')} title=' .' />
+                        <Spoiler post text={checkParagraph(t('userAgreementText'))} title=' .' />
                     </section>
                 </div>
                 <Footer />
@@ -29,7 +39,7 @@ const InformationPage = () => {
                 <div className="container">
                     <section className="content">
                         <SectionTitle title={t('PrivacyTitle')} />
-                        <Spoiler post text={t('PrivacyText')} title=' .' />
+                        <Spoiler post text={checkParagraph(t('PrivacyText'))} title=' .' />
                     </section>
                 </div>
                 <Footer />
